3da62c560366313c9de9555dacb27df04fdf43c6
GHDB ASHEN <your-email@example.com>
2024-12-22 19:50:47 +0530

    post response

--- whatsapp.py
+++ whatsapp.py
@@ -21,8 +21,14 @@ def get_message():
     else:
         print("Smiley image not found.")
         return ""
+def post_response(message):
+    """Post a response in WhatsApp."""
+    position1 = pt.locateOnScreen("smiley.png", confidence=0.6)
+    if position1:
+        x, y = position1[0], position1[1]
+        pt.moveTo(x + 200, y + 20, duration=0.5)
+        pt.click()
+        pt.typewrite(message, interval=0.01)
+        pt.typewrite("\n", interval=0.01)
 
 
-
-git config --global user.name "GHDB ASHEN"
-git config --global user.email "ghdbashen@gmail.com"
